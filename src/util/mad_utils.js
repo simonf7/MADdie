a64e94b93917dacb97f8329588d276f84cc559e1
Simon Foster <simonf7@hotmail.com>
2020-05-26 13:21:36 +0100

    Feat: add quests and areas command

--- src/util/mad_utils.js
+++ src/util/mad_utils.js
@@ -1,5 +1,38 @@
 const fetch = require('node-fetch');
 
+const fetchJson = async (url) => {
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        console.log(url + ': ' + res.statusText + ' (' + res.status + ')');
+        return {
+          error: res.status,
+          message: res.statusText,
+        };
+      }
+      return res.json();
+    })
+    .then((json) => {
+      return json;
+    })
+    .catch((err) => {
+      console.log(url + ': ' + err);
+      return {
+        error: 500,
+        message: err,
+      };
+    });
+};
+
+const fetchResults = async (url) => {
+  return fetchJson(url).then((json) => {
+    if (json.results) {
+      return json.results;
+    }
+    return json;
+  });
+};
+
 const getShinyStats = async (client, timeFrom, timeTo) => {
   const url =
     client.config.mad.host +
@@ -8,41 +41,40 @@ const getShinyStats = async (client, timeFrom, timeTo) => {
     '&to=' +
     timeTo.format('X');
 
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      return json;
-    });
+  return fetchJson(url);
 };
 
 const getStatus = async (client) => {
   const url = client.config.mad.host + '/get_status';
 
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      return json;
-    });
+  return fetchJson(url);
 };
 
 const getDevices = async (client) => {
   const url = client.config.mad.host + '/api/device';
 
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      return json.results;
-    });
+  return fetchResults(url);
 };
 
 const getWalkers = async (client) => {
   const url = client.config.mad.host + '/api/walker';
 
-  return fetch(url)
-    .then((res) => res.json())
-    .then((json) => {
-      return json.results;
-    });
+  return fetchResults(url);
+};
+
+const getGeofences = async (client) => {
+  const url = client.config.mad.host + '/api/geofence';
+
+  return fetchResults(url);
+};
+
+const getQuests = async (client, fence) => {
+  let url = client.config.mad.host + '/get_quests';
+  if (fence !== '') {
+    url = url + '?fence=' + fence;
+  }
+
+  return fetchJson(url);
 };
 
 module.exports = {
@@ -50,4 +82,6 @@ module.exports = {
   getStatus,
   getDevices,
   getWalkers,
+  getGeofences,
+  getQuests,
 };
