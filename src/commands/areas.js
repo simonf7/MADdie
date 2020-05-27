2cf03673acb329bd6a65359dd76afd2b07715962
Simon Foster <simonf7@hotmail.com>
2020-05-27 12:53:25 +0100

    Feat: show areas in a table

--- src/commands/areas.js
+++ src/commands/areas.js
@@ -1,22 +1,21 @@
 exports.run = async (client, msg, args) => {
   client.madUtils.getQuestStats(client).then((questStats) => {
-    let text = '';
+    let table = [];
+    table.push(['Area', 'Processed', 'Quests', 'Stops']);
 
     questStats.stop_quest_stats.forEach((a) => {
-      text =
-        text +
-        (text === '' ? '' : '\n') +
-        '**' +
-        a.fence.replace('pokestops_', '') +
-        '**  processed: ' +
-        a.processed +
-        ' (' +
-        a.quests +
-        ' / ' +
-        a.stops +
-        ')';
+      table.push([
+        a.fence.replace('pokestops_', ''),
+        a.processed.replace(' ', '').padStart(4, ' '),
+        a.quests.toString().padStart(3, ' '),
+        a.stops.toString().padStart(3, ' '),
+      ]);
     });
 
-    msg.reply(client.discordUtils.msgEmbed(text));
+    msg.reply(
+      client.discordUtils.msgEmbed(
+        client.discordUtils.showTable(table, ['l', 'r', 'r', 'r'])
+      )
+    );
   });
 };
