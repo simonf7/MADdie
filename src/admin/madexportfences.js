6d372444ec587bdf40bd762402c10651239e0048
Simon Foster <simonf7@hotmail.com>
2020-05-27 17:09:32 +0100

    Feat: allow include/exclude areas in export

--- src/admin/madexportfences.js
+++ src/admin/madexportfences.js
@@ -1,6 +1,18 @@
 const fs = require('fs');
 
 exports.run = async (client, msg, args) => {
+  // split args between include and exclude
+  const inc = args
+    .filter((a) => {
+      return a.startsWith('!') == false;
+    })
+    .map((a) => a.replace(' ', '_'));
+  const exc = args
+    .filter((a) => {
+      return a.startsWith('!');
+    })
+    .map((a) => a.replace(' ', '_'));
+
   const fences = await client.madUtils.getGeofences(client);
   let output = [];
 
@@ -24,22 +36,37 @@ exports.run = async (client, msg, args) => {
 
   for (var key in fences) {
     const fence = await client.madUtils.fetchResults(client, key);
-    const id = key.match(/[0-9]+/g);
-    let json = {
-      name: fence.name,
-      color: colors[count % colors.length],
-      id: parseInt(id[0]),
-      path: [],
-    };
-    fence.fence_data.forEach((c) => {
-      const location = c.match(/[0-9]+[.][0-9]+/g);
-      if (location && location.length == 2) {
-        json.path.push(location.map((l) => parseFloat(l)));
+
+    let include = inc.length == 0 ? true : exc.length == 0 ? false : true;
+    inc.forEach((a) => {
+      if (fence.name.startsWith(a)) {
+        include = true;
       }
     });
-    output.push(json);
+    exc.forEach((a) => {
+      if (fence.name.startsWith(a.substring(1))) {
+        include = false;
+      }
+    });
 
-    count += 1;
+    if (include) {
+      const id = key.match(/[0-9]+/g);
+      let json = {
+        name: fence.name,
+        color: colors[count % colors.length],
+        id: parseInt(id[0]),
+        path: [],
+      };
+      fence.fence_data.forEach((c) => {
+        const location = c.match(/[0-9]+[.][0-9]+/g);
+        if (location && location.length == 2) {
+          json.path.push(location.map((l) => parseFloat(l)));
+        }
+      });
+      output.push(json);
+
+      count += 1;
+    }
   }
 
   fs.writeFileSync(
